71bf740512db4b21342086e816d82801cf1dfac6
angeliarbol55 <angeliarboleda74@gmail.com>
2026-03-06 13:15:29 -0500

    Update 1 eva.py

---
+++ 1 eva.py
@@ -0,0 +1,5 @@
+num_1=int(input("Introduce el primer número: "))
+num_2=int(input("Introduce el segundo número: "))
+num_3=int(input("Introduce el tercer número: "))
+suma=num_1+num_2+num_3
+print("La suma de los tres números es: ", suma)
