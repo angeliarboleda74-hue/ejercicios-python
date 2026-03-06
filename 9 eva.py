4aa5fd17605afc9001238e8a7d3cbc35b03ba4ef
angeliarbol55 <angeliarboleda74@gmail.com>
2026-03-06 13:26:12 -0500

    Add commission calculation for employee salary

---
+++ 9 eva.py
@@ -0,0 +1,8 @@
+sueldo_base=float(input("Ingrese el sueldo base del empleado: "))
+v1=float(input("venta 1:"))
+v2=float(input("venta 2:"))
+v3=float(input("venta 3:")) 
+comisiones=(v1+v2+v3)*0.10
+total=sueldo_base+comisiones
+print("Las comisiones son:", comisiones)
+print("El sueldo total es:", total)
