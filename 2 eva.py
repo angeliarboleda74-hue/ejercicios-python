716b3eb24584d0c2e5c845248afd29a9d83b61fb
angeliarbol55 <angeliarboleda74@gmail.com>
2026-03-06 13:20:14 -0500

    Add basic arithmetic operations in eva.py

---
+++ 2 eva.py
@@ -0,0 +1,10 @@
+num_1=int(input("Ingrese el primer número: "))
+num_2=int(input("Ingrese el segundo número: "))
+suma=num_1+num_2
+resta=num_1-num_2
+multiplicacion=num_1*num_2
+division=num_1/num_2
+print("La suma es: ", suma)
+print("La resta es: ", resta)
+print("La multiplicación es: ", multiplicacion)
+print("La división es: ", division)
