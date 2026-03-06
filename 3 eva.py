c994a303770522392be3e076108df9a6147f32ec
angeliarbol55 <angeliarboleda74@gmail.com>
2026-03-06 13:23:07 -0500

    Add script to calculate average of three grades

---
+++ 3 eva.py
@@ -0,0 +1,6 @@
+nota_1=float(input("Ingrese la primera nota: "))
+nota_2=float(input("Ingrese la segunda nota: "))
+nota_3=float(input("Ingrese la tercera nota: "))
+suma=nota_1+nota_2+nota_3
+definitiva=suma/3
+print("La nota definitiva es: ", definitiva)
